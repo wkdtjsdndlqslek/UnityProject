750839cb39132a6bd351362b498fe50b8ffd5939
wkdtjsdndlqslek <ehdtk456@naver.com>
2024-10-29 22:54:35 +0900

    카메라 좌우 전환 완료

--- Unity_1st_Project/Assets/Scripts/System/CameraCtrl.cs
+++ Unity_1st_Project/Assets/Scripts/System/CameraCtrl.cs
@@ -13,16 +13,24 @@ public class CameraCtrl : MonoBehaviour
     private void Update()
     {
         Vector3 mousePosition = Input.mousePosition;
-
         float screenWidth = Screen.width;
 
-        if(mousePosition.x < edgeThreshold)
+        if (mousePosition.x < edgeThreshold)
         {
             transform.position +=Vector3.left*moveSpeed*Time.deltaTime;
+            if (transform.position.x < -4.73f)
+            {
+                transform.position = new Vector3(-4.73f, 0, -1);
+            }
         }
-        else if(mousePosition.x > screenWidth-edgeThreshold)
+
+        else if (mousePosition.x > screenWidth-edgeThreshold)
         {
             transform.position +=Vector3.right*moveSpeed*Time.deltaTime;
+            if (transform.position.x > 4.73f)
+            {
+                transform.position = new Vector3(4.73f, 0, -1);
+            }
         }
     }
 }
