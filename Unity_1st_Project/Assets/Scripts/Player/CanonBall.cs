115c1c4135d06a83647b76a2801ae11f02a6100b
wkdtjsdndlqslek <ehdtk456@naver.com>
2024-11-04 21:09:43 +0900

    대포 기믹 완료!

--- Unity_1st_Project/Assets/Scripts/Player/CanonBall.cs
+++ Unity_1st_Project/Assets/Scripts/Player/CanonBall.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Transactions;
+using UnityEditor;
 using UnityEngine;
 
 public class CanonBall : MonoBehaviour
 {
-    public float damage = 10f;
+    public int damage = 2000;
     public float moveSpeed = 5;
+    public float explosionRange = 2f;
+    public GameObject explosion;
+
+    CircleCollider2D circleCollider;
     private void Start()
     {
     }
@@ -21,7 +27,31 @@ public class CanonBall : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        collision.CompareTag("Land");
-        Destroy(gameObject);
+        if(collision.CompareTag("Land"))
+        {
+            print(transform.position);
+            Instantiate(explosion,transform.position,Quaternion.identity);
+            RaycastHit2D[] hit2Ds = Physics2D.CircleCastAll(transform.position, explosionRange, Vector2.zero);
+
+            Vector2[] coordination = new Vector2[360];
+            for(int i = 0; i<360; i++)
+            {
+                coordination[i]=new Vector2(Mathf.Cos(i),Mathf.Sin(i))*explosionRange;
+                if (i<359)
+                {
+                Debug.DrawLine(coordination[i]+(Vector2)transform.position, coordination[i+1]+(Vector2)transform.position);
+                }
+            }
+            foreach (RaycastHit2D hit2d in hit2Ds)
+            {
+                if(hit2d.collider.CompareTag("Enemy"))
+                {
+                    hit2d.collider.GetComponent<EnemyUnit>().TakeDamage(damage);
+                }
+            }
+
+            Destroy(gameObject);
+        }
+        
     }
 }
