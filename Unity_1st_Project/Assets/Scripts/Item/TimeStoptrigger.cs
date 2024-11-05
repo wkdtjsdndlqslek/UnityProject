0cca8fd539f36e7cd9d1600fa6ac510904114fc5
wkdtjsdndlqslek <ehdtk456@naver.com>
2024-11-05 18:01:53 +0900

    timeStop 완료

--- Unity_1st_Project/Assets/Scripts/Item/TimeStoptrigger.cs
+++ Unity_1st_Project/Assets/Scripts/Item/TimeStoptrigger.cs
@@ -1,14 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TimeStoptrigger : MonoBehaviour
 {
+    public TextMeshProUGUI timer;
+    private int min;
+    private float sec;
+    private float stopTimer = 10;
+    private float milliSec;
+
     private void OnEnable()
     {
         StartCoroutine(BreakStop());
     }
 
+    private void Update()
+    {
+        stopTimer -=Time.deltaTime;
+        sec = (int)stopTimer;
+        milliSec =(int)((stopTimer -sec)*100);
+        timer.text = $"{sec}:{milliSec}";
+    }
     IEnumerator BreakStop()
     {
         GameManager.Instance.isTimeStop = true;
